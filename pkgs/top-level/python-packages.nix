ca2ff81ec9860666892e998713e3ade5ed457985
phaer <hello@phaer.org>
2022-06-02 12:38:26 +0200

    python3Packages.segno: init at 1.5.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -9585,6 +9585,8 @@ in {
 
   segments = callPackage ../development/python-modules/segments { };
 
+  segno = callPackage ../development/python-modules/segno { };
+
   segyio = toPythonModule (callPackage ../development/python-modules/segyio {
     inherit (pkgs) cmake ninja;
   });
